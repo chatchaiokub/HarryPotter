0338e7c6c4ab49583ca294ba7ddbcd403f785830
Chatchaiokub <chatchaiokub@Chatchaiokubs-MacBook-Pro.local>
2016-03-24 03:49:58 +0700

    fix

--- public/app.js
+++ public/app.js
@@ -1,46 +1,40 @@
 var angular = angular.module('order', [])
   .controller('orderController', function ($scope) {
     $scope.ORDER = []
-    $scope.add = function (data) {
-      if (check(data)) {
-        var count = find(data)
-        $scope.ORDER[count].amount++
+    $scope.addOrder = function (data) {
+      if (checkRepeat(data)) {
+        var find = findRepeat(data)
+        $scope.ORDER[find].amount++
       } else {
         $scope.ORDER.push(data)
-      // console.log($scope.ORDER)
       }
-      $scope.sum = promotion($scope.ORDER)
+      $scope.Total = promotion($scope.ORDER)
     }
-    var find = function (data) {
-      for (var i = 0; i < $scope.ORDER.length; i++) {
-        if ($scope.ORDER[i].sector === data.sector) {
-          return (i)
-        }
-      // console.log('find')
-      }
-    }
-    var check = function (data) {
+    var checkRepeat = function (data) {
       for (var i = 0; i < $scope.ORDER.length; i++) {
         if ($scope.ORDER[i].sector === data.sector) {
           return (true)
         }
-      // console.log('check')
       }
       return (false)
     }
-
+    var findRepeat = function (data) {
+      for (var i = 0; i < $scope.ORDER.length; i++) {
+        if ($scope.ORDER[i].sector === data.sector) {
+          return (i)
+        }
+      }
+    }
     var promotion = function (order) {
       var length = order.length
       var price = 0
       for (var i = 0; i < order.length; i++) {
         price += order[i].amount * 100
-      // console.log('price')
       }
-      var discount = 0
       var delAmount = []
-
-      for (var p = 0; p < order.length; p++) {
-        delAmount.push({price: order[p].price, sector: order[p].sector, amount: order[p].amount})
+      var discount = 0
+      for (var x = 0; x < order.length; x++) {
+        delAmount.push({price: order[x].price, sector: order[x].sector, amount: order[x].amount})
       }
 
       while (length > 1) {
@@ -58,21 +52,17 @@ var angular = angular.module('order', [])
           discount += 420
         }
 
-        for (var a = 0; a < delAmount.length; a++) {
-          delAmount[a].amount--
-        // console.log('hey')
+        for (var l = 0; l < delAmount.length; l++) {
+          delAmount[l].amount--
         }
-        for (var j = delAmount.length - 1; j >= 0; j--) {
-          if (delAmount[j].amount === 0) {
-            delAmount.splice(j, 1)
-            console.log('splice')
+        for (var s = delAmount.length - 1; s >= 0; s--) {
+          if (delAmount[s].amount === 0) {
+            delAmount.splice(s, 1)
           }
         }
-        // delAmount = delAmount.filter((element) => element.amount !== 0)
 
         length = delAmount.length
       }
-      console.log(price - discount)
-      return ({discount: discount, sum: price - discount})
+      return ({discount: discount, sumPrice: price - discount})
     }
   })
